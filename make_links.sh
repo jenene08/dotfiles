4624968a1597eb2fac4ade8213d0f83348831591
jenene08 <33044125+jenene08@users.noreply.github.com>
2018-01-31 23:15:34 +0900

    modified make_links.sh : use loop for everything in this file

--- make_links.sh
+++ make_links.sh
@@ -1,29 +1,12 @@
-#!/bin/sh
+#!/usr/local/bin/zsh
 
-#bash
-ln -si "${0}/.bashrc" "${HOME}/.bashrc"
-ln -si "${0}/.bash_profile" "${HOME}/.bash_profile"
+cd `dirname ${0}`
+for file in `find . -type f`; do
+  echo "$(dirname ${0})/${file}"
+  if [ `echo $file | grep ".git/\|.DS_Store\|make_links.sh"` ] ; then
+    continue
+  else
+    ln -si "$(dirname ${0})/${file}" "${HOME}/${file}"
+  fi
+done
 
-#zsh
-ln -si "${0}/.zshrc" "${HOME}/.zshrc"
-ln -si "${0}/.zshenv" "${HOME}/.zshenv"
-
-#ipython
-# ln -si "${0}/.ipython" "${HOME}/.ipython"
-# ln i-s "${0}/.ipython/profile_default" "${HOME}/.ipython/profile_default"
-# ln i-s ./.ipython/profile_default/startup ${HOME}/./.ipython/profile_default/startup"
-ln -si "${0}/.ipython/profile_default/startup/01.py" "${HOME}/.ipython/profile_default/startup/01.py"
-
-#git
-ln -si "${0}/.gitignore_global" "${HOME}/.gitignore_global"
-ln -si "${0}/.gitattributes" "${HOME}/.gitattributes"
-ln -si "${0}/.gitconfig" "${HOME}/.gitconfig"
-
-#vim
-ln -si "${0}/.vimrc" "${HOME}/.vimrc"
-
-#latex
-ln -si "${0}/.latexmkrc" "${HOME}/.latexmkrc"
-
-#gnuplot
-ln -si "${0}/.gnuplot" "${HOME}/.gnuplot"
